83125b2e2823a84af3ec6b4a843f3945abed1df7
JunichiroKohari <junichiro@kohari.jp>
2022-02-19 19:50:40 +0900

    write first spec ts

---
+++ cypress/integration/first.spec.ts
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe('example to-do app', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('初めてのcypressテスト', () => {
+    cy.get('.title').should('have.text', 'sp-view-service')
+  })
+
+})
